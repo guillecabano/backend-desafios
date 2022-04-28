6ec972ec84bbcf3ade4dd94bfa47b4d1f6e61938
Guille Cabano <guillecabano@gmail.com>
2022-04-28 16:27:21 -0300

    Se agrega el desafio 5

--- Desafio 4/index.js
+++ Desafio 4/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const apiRouter = require('./routes/productos.routes')
+const apiRouter = require('../routes/productos.routes')
 const PORT = 8080
 
 app.use(express.json())
